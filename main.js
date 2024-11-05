68b72c9a16488f23ae4902b5394df8512b5a18eb
Johannes <johannes.persson@lerniastudent.se>
2024-11-05 23:35:32 +0100

    051124

---
+++ main.js
@@ -0,0 +1,15 @@
+ const hamburger = document.querySelector('.hamburger__pic');
+ const closeBtn = document.querySelector('.close');
+ const menu = document.querySelector('.menu__container');
+ const body = document.body;
+
+ hamburger.addEventListener('click', ()=> {
+    menu.style.display = 'block';
+    hamburger.style.display = 'none';
+    body.style.position = 'fixed';
+});
+closeBtn.addEventListener('click', ()=> {
+    menu.style.display = 'none';
+    hamburger.style.display = 'block';
+    body.style.position = 'unset';
+});
