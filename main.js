66755b6d835adf9d1db7086366500601e13de119
Johannes <johannes.persson@lerniastudent.se>
2024-11-05 23:39:44 +0100

    .

--- main.js
+++ main.js
@@ -7,9 +7,11 @@
     menu.style.display = 'block';
     hamburger.style.display = 'none';
     body.style.position = 'fixed';
-});
+ }
+);
 closeBtn.addEventListener('click', ()=> {
     menu.style.display = 'none';
     hamburger.style.display = 'block';
     body.style.position = 'unset';
-});
+ }
+);
