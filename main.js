a891f38f8143d7c1208fba1f9e9d5e2bda47cc62
Johannes <johannes.persson@lerniastudent.se>
2024-11-06 12:26:51 +0100

    adding som comments

--- main.js
+++ main.js
@@ -3,15 +3,17 @@
  const menu = document.querySelector('.menu__container');
  const body = document.body;
 
+ //show menu when clicking on hamburger icon
  hamburger.addEventListener('click', ()=> {
     menu.style.display = 'block';
     hamburger.style.display = 'none';
-    body.style.position = 'fixed';
+    body.style.position = 'fixed';//fix the page content to prevent scrolling
  }
 );
+//close menu when clicking on close button
 closeBtn.addEventListener('click', ()=> {
     menu.style.display = 'none';
     hamburger.style.display = 'block';
-    body.style.position = 'unset';
+    body.style.position = 'unset'; //restore scrolling on the page
  }
 );
