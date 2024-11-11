534ff4672466f5d21852d90f7c612be51b3d4dc4
Johannes <johannes.persson@lerniastudent.se>
2024-11-11 09:55:33 +0100

    deleted some comments

--- main.js
+++ main.js
@@ -7,13 +7,12 @@
  hamburger.addEventListener('click', ()=> {
     menu.style.display = 'block';
     hamburger.style.display = 'none';
-    //body.style.position = 'fixed';//fix the page content to prevent scrolling
  }
 );
 //close menu when clicking on close button
 closeBtn.addEventListener('click', ()=> {
     menu.style.display = 'none';
     hamburger.style.display = 'block';
-    //body.style.position = 'unset'; //restore scrolling on the page
+    
  }
 );
