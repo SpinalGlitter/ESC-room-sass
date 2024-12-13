a943ca86137384b4f8924e4d935c8593938b3742
Johannes <johannes.persson@lerniastudent.se>
2024-12-13 16:26:50 +0100

    test git hooks

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
- const hamburger = document.querySelector('.hamburger__pic');
- const closeBtn = document.querySelector('.close');
- const menu = document.querySelector('.menu__container');
+const hamburger = document.querySelector('.hamburger__pic');
+const closeBtn = document.querySelector('.close');
+const menu = document.querySelector('.menu__container');
 
- //show menu when clicking on hamburger icon
- hamburger.addEventListener('click', ()=> {
-    menu.style.display = 'block';
-    hamburger.style.display = 'none';
- }
-);
+//show menu when clicking on hamburger icon
+hamburger.addEventListener('click', () => {
+  menu.style.display = 'block';
+  hamburger.style.display = 'none';
+  document.body.style.overflow = 'hidden';
+});
 //close menu when clicking on close button
-closeBtn.addEventListener('click', ()=> {
-    menu.style.display = 'none';
-    hamburger.style.display = 'block';
- }
-);
+closeBtn.addEventListener('click', () => {
+  menu.style.display = 'none';
+  hamburger.style.display = 'block';
+  document.body.style.overflow = 'auto';
+});
